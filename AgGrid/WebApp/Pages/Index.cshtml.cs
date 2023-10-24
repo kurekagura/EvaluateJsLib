75a4d03090771ab48c0e79bd6b39853ed618b591
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-10-24 23:23:29 +0900

    サンプルデータをjsonから取得して表示。

--- AgGrid/WebApp/Pages/Index.cshtml.cs
+++ AgGrid/WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _env;
+        public List<Deposit> DepositList;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            string jsonPath = Path.Combine(_env.WebRootPath, "deposit.json");
 
+            // JSON データを List<Deposit> に逆シリアル化
+            var jsonStr = System.IO.File.ReadAllText(jsonPath);
+            DepositList = JsonSerializer.Deserialize<List<Deposit>>(jsonStr) ?? new List<Deposit>();
+
+            return Page();
         }
+    }
+
+    public class Deposit
+    {
+        [JsonPropertyName("RegistrationDate")]
+        public DateTime RegistrationDate { get; set; }
+        [JsonPropertyName("UserID")]
+        public required string UserID { get; set; }
+        [JsonPropertyName("Name")]
+        public required string Name { get; set; }
+        [JsonPropertyName("Gender")]
+        public string? Gender { get; set; }
+        [JsonPropertyName("Age")]
+        public int Age { get; set; }
+        [JsonPropertyName("Total")]
+        public decimal Total { get; set; }
+        [JsonPropertyName("Birthday")]
+        public DateTime Birthday { get; set; }
     }
 }
