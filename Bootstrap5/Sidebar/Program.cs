72cf20b60051619531a9e7765bd554bed4fcaea5
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-11-09 21:53:03 +0900

    更新。

--- Bootstrap5/Sidebar/Program.cs
+++ Bootstrap5/Sidebar/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Localization;
 using System.Globalization;
 
 namespace Sidebar
@@ -8,19 +9,21 @@ namespace Sidebar
         {
             var builder = WebApplication.CreateBuilder(args);
             //↓役割謎。無くても機能しているような・・・。
-            //builder.Services.AddLocalization(options => options.ResourcesPath = "MyProjectResources");
+            builder.Services.AddLocalization(options => options.ResourcesPath = "MyResourceNamespace");
             builder.Services.AddHttpContextAccessor();
             // Add services to the container.
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
-            var supportedCultures = new List<CultureInfo> { new CultureInfo("ja-JP") };
-            app.UseRequestLocalization(new RequestLocalizationOptions
+            var supportedCultures = new List<CultureInfo> { new CultureInfo("ja") };
+            var reqLocOpts = new RequestLocalizationOptions
             {
                 //DefaultRequestCulture = new RequestCulture("ja-JP"),
+                DefaultRequestCulture = new RequestCulture(CultureInfo.InvariantCulture),
                 SupportedCultures = supportedCultures,
                 SupportedUICultures = supportedCultures
-            });
+            };
+            app.UseRequestLocalization(reqLocOpts);
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
