0c9a0e764b433532426db432dc3e2c5e78d4d19c
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-11-30 23:37:45 +0900

    Spinner用例。

---
+++ Bootstrap5/Bootstrap/Pages/Spinner/Spinner1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bootstrap.Pages.Spinner
+{
+    public class Spinner1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
