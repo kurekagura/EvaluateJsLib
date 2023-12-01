98064a4dee626684b55620c8973a6eb45dbc4a2b
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-02 07:39:24 +0900

    JQueryプラグイン実装テスト用。

---
+++ JQuery/WebApp/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿//(function ($) {
+//    $.fn.myMenu = function (options) {
+//        var settings = $.extend({
+//            items: [] // メニュー項目の配列
+//        }, options);
+
+//        var selectedValue = null;
+
+//        function getSelectedValue() {
+//            return selectedValue;
+//        }
+
+//        // メニューの生成
+//        var $ul = this.find('.dropdown-menu');
+//        $.each(settings.items, function (index, item) {
+//            var $li = $('<li></li>');
+//            var $button = $('<button class="dropdown-item" type="button"></button>')
+//                .attr('data-value', item.value)
+//                .text(item.name)
+//                .on('click', function () {
+//                    selectedValue = item.value;
+//                    $ul.trigger('selected.mymenu', [selectedValue]);
+//                });
+//            $li.append($button);
+//            $ul.append($li);
+//        });
+
+//        return this;
+//    };
+//})(jQuery);
+
+(function ($) {
+    $.fn.myMenu = function () {
+        return this.each(function () {
+            var $li = $(this);
+
+            var selectedValue = $li.find('button.dropdown-item').attr('data-value');
+
+            $li.on('click', 'button.dropdown-item', function () {
+                var clickedValue = $(this).attr('data-value');
+                $li.trigger('selected.mymenu', [clickedValue]);
+            });
+        });
+    };
+})(jQuery);
