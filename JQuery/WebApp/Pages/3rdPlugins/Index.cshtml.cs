0d0d5f205cbdb1f96c9064a19f21763f4b3f7897
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-13 23:02:21 +0900

    select2とbootstrap-input-spinnerのお試しコード作成。

---
+++ JQuery/WebApp/Pages/3rdPlugins/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages._3rdPlugins
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ JQuery/WebApp/Pages/3rdPlugins/Select2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages._3rdPlugins
+{
+    public class Select2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
