00fe17f6b922a218a994d0287c00807b52cfea87
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-16 02:15:24 +0900

    cropperjsのお試しコード作成。

---
+++ Cropperjs/WebApp/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace WebApp;
+
+public static class EnumExtentions
+{
+    public static string DisplayName(this Enum value)
+    {
+        if (!Enum.IsDefined(value.GetType(), value))
+            throw new InvalidEnumArgumentException();
+
+
+        var fieldInfo = value.GetType().GetField(value.ToString())!;
+
+        if (fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false)?.FirstOrDefault() is DisplayAttribute attribute)
+        {
+            //Display属性が定義してある場合（Name=,ResourceType=必須）
+            var nameProperty = attribute.ResourceType!.GetProperty(attribute.Name!, BindingFlags.Static | BindingFlags.Public);
+            var name = nameProperty!.GetValue(nameProperty.DeclaringType, null) as string;
+            return name!;
+        }
+        else
+        {
+            //Display属性が定義していなかった場合、Enumの名称を返す
+            return value.ToString();
+        }
+
+    }
+}
---
+++ Cropperjs/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<EnumCropRatio> CropRatioRadios { set; get; } = new();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+            CropRatioRadios.AddRange(new[] {
+            EnumCropRatio.OneToOne, EnumCropRatio.SixteenToNine, EnumCropRatio.Free});
+        }
+    }
+
+    public enum EnumCropRatio
+    {
+        [Display(Name = "1:1")]
+        OneToOne,
+        [Display(Name = "16:9")]
+        SixteenToNine,
+        [Display(Name = "Free")]
+        Free
+    }
+
+}
