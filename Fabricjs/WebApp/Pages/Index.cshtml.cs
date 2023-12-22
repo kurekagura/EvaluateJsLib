38dcbc8142bfe21aab06aac084d43ab3219359d9
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-23 03:41:21 +0900

    Fabric.jsお試し。

---
+++ Fabricjs/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        //public List<EnumCropRatio> CropRatioRadios { set; get; } = new();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //CropRatioRadios.AddRange(new[] { EnumCropRatio.OneToOne, EnumCropRatio.SixteenToNine, EnumCropRatio.Free });
+        }
+    }
+
+    //public enum EnumCropRatio
+    //{
+    //    [Display(Name = "1:1")]
+    //    OneToOne,
+    //    [Display(Name = "16:9")]
+    //    SixteenToNine,
+    //    [Display(Name = "Free")]
+    //    Free
+    //}
+}
