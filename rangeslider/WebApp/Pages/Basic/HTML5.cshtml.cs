9e050a94da7594607ff35afe25fd77bd19884db7
kurekagura <26186258+kurekagura@users.noreply.github.com>
2024-01-28 00:23:30 +0900

    HTML5のrange。

---
+++ rangeslider/WebApp/Pages/Basic/HTML5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Basic
+{
+    public class HTML5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
